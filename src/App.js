f796f2c48e881c38705334b7f5d56875bc11b8c2
Nilay Sondagar <nilay@fispan.com>
2021-09-19 13:20:42 -0700

    - base csc375 site

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import {
+  Switch,
+  Redirect,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.scss';
+
+import AOS from 'aos';
+import { focusHandling } from 'cruip-js-toolkit';
+import smoothscroll from 'smoothscroll-polyfill';
+
+import Home from './pages/Home';
+
+function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    smoothscroll.polyfill();
+
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 700,
+      easing: 'ease-out-cubic',
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'smooth'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = 'smooth'
+    focusHandling('outline');
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
